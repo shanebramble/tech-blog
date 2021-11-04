8c6183f072764082c31ae45edb0a8e82e2cae752
Shane Bramble-Wade <sbramwade@msn.com>
2021-11-03 20:19:52 -0500

    started the implementation of the homepage template

---
+++ controllers/html/home-routes.js
@@ -0,0 +1,7 @@
+const router = require ('express').Router()
+const sequelize = require('../../config/connection');
+const {User} = require ('../../models');
+
+router.get('/', (req,res) => {
+    res.render('homepage');
+});
